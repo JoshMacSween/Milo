faf64182f853fb7e503554421d060819c67a1b9a
Josh MacSween <joshmacsween@gmail.com>
2024-01-10 16:07:11 -0700

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
   post '/graphql', to: 'graphql#execute'
+  get '/about', to: 'pages#about'
   root 'pages#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
